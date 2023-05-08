de295aab248c6bd17e9c40b0c912c8d7852d7e2b
CarlosCab19 <CabTunCarlos@gmail.com>
2023-05-07 22:28:11 -0600

    link
    link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title><a href="https://www.youtube.com/watch?v=dSqsXku9WaM&ab_channel=RichardAhumada">Hello World</a></Title>
-      <h1>Hello world!!</h1>
+      <Title>Hello World</Title>
+      <h1><a href="https://www.youtube.com/watch?v=dSqsXku9WaM&ab_channel=RichardAhumada">Hello world!!</a>></h1>
       <Counter />
       <p>
         <a href="https://www.youtube.com/watch?v=dSqsXku9WaM&ab_channel=RichardAhumada" target="_blank">
